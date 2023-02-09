51c9c0d5075c9d47e94625739057cbc74e2446b5
antoniotarricone <antonio.tarricone@pagopa.it>
2023-02-09 12:45:54 +0100

    Container App renamed.

--- src/core/05_container_apps_mil_functions.tf
+++ src/core/05_container_apps_mil_functions.tf
@@ -1,14 +1,14 @@
 # Container App for mil-functions
 locals {
-  name = "${local.project}-functions-ca"
+  mil_functions_ca_name = "${local.project}-functions-ca"
 }
 
 resource "azurerm_resource_group_template_deployment" "mil_functions" {
-  name                = local.name
+  name                = local.mil_functions_ca_name
   resource_group_name = azurerm_resource_group.app.name
   deployment_mode     = "Incremental"
   tags                = var.tags
-  
+
   lifecycle {
     ignore_changes = [
       template_content
@@ -17,22 +17,22 @@ resource "azurerm_resource_group_template_deployment" "mil_functions" {
 
   template_content = templatefile("templates/mil-functions.json",
     {
-      name                                         = local.name,
-      location                                     = azurerm_resource_group.app.location,
-      mongo_connection_string_1                    = azurerm_cosmosdb_account.mil.connection_strings[0],
-      mongo_connection_string_2                    = azurerm_cosmosdb_account.mil.connection_strings[1],
-      managed_environment_id                       = module.cae.id,
-      mil_functions_image                          = var.mil_functions_image,
-      mil_functions_quarkus_log_level              = var.mil_functions_quarkus_log_level,
-      mil_functions_app_log_level                  = var.mil_functions_app_log_level,
-      mil_functions_mongo_connect_timeout          = var.mil_functions_mongo_connect_timeout,
-      mil_functions_mongo_read_timeout             = var.mil_functions_mongo_read_timeout,
-      mil_functions_mongo_server_selection_timeout = var.mil_functions_mongo_server_selection_timeout,
-      mil_functions_cpu                            = var.mil_functions_cpu,
-      mil_functions_ephemeral_storage              = var.mil_functions_ephemeral_storage,
-      mil_functions_memory                         = var.mil_functions_memory,
-      mil_functions_max_replicas                   = var.mil_functions_max_replicas,
-      mil_functions_min_replicas                   = var.mil_functions_min_replicas
+      name                           = local.mil_functions_ca_name,
+      location                       = azurerm_resource_group.app.location,
+      mongo_connection_string_1      = azurerm_cosmosdb_account.mil.connection_strings[0],
+      mongo_connection_string_2      = azurerm_cosmosdb_account.mil.connection_strings[1],
+      managed_environment_id         = module.cae.id,
+      image                          = var.mil_functions_image,
+      quarkus_log_level              = var.mil_functions_quarkus_log_level,
+      app_log_level                  = var.mil_functions_app_log_level,
+      mongo_connect_timeout          = var.mil_functions_mongo_connect_timeout,
+      mongo_read_timeout             = var.mil_functions_mongo_read_timeout,
+      mongo_server_selection_timeout = var.mil_functions_mongo_server_selection_timeout,
+      cpu                            = var.mil_functions_cpu,
+      ephemeral_storage              = var.mil_functions_ephemeral_storage,
+      memory                         = var.mil_functions_memory,
+      max_replicas                   = var.mil_functions_max_replicas,
+      min_replicas                   = var.mil_functions_min_replicas
     }
   )
 }
