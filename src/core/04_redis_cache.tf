3ed48feacd43eff4350aa1ec1878c39b62f67e81
antoniotarricone <antonio.tarricone@pagopa.it>
2023-02-13 17:28:56 +0100

    Public network access enabled (private link is temporarily disabled).

--- src/core/04_redis_cache.tf
+++ src/core/04_redis_cache.tf
@@ -1,14 +1,15 @@
 # Redis Cache.
 module "redis_cache" {
-  source                = "git::https://github.com/pagopa/terraform-azurerm-v3.git//redis_cache?ref=v4.1.12"
-  name                  = "${local.project}-redis"
-  resource_group_name   = azurerm_resource_group.data.name
-  location              = azurerm_resource_group.data.location
-  capacity              = 1
-  enable_non_ssl_port   = false
-  family                = "C"
-  sku_name              = "Basic"
-  enable_authentication = true
+  source                        = "git::https://github.com/pagopa/terraform-azurerm-v3.git//redis_cache?ref=v4.1.12"
+  name                          = "${local.project}-redis"
+  resource_group_name           = azurerm_resource_group.data.name
+  location                      = azurerm_resource_group.data.location
+  capacity                      = 1
+  enable_non_ssl_port           = false
+  family                        = "C"
+  sku_name                      = "Basic"
+  enable_authentication         = true
+  public_network_access_enabled = true
 
   private_endpoint = {
     enabled              = false
