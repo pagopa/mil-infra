d5a27bfbe11e0efe08696024fbd305f7a3370797
antoniotarricone <antonio.tarricone@pagopa.it>
2023-02-23 17:17:19 +0100

    Private endpoint added.

--- src/core/04_cosmosdb.tf
+++ src/core/04_cosmosdb.tf
@@ -1,11 +1,12 @@
 # Account
 resource "azurerm_cosmosdb_account" "mil" {
-  name                = "${local.project}-cosmos"
-  resource_group_name = azurerm_resource_group.data.name
-  location            = azurerm_resource_group.data.location
-  kind                = "MongoDB"
-  offer_type          = "Standard"
-  tags                = var.tags
+  name                          = "${local.project}-cosmos"
+  resource_group_name           = azurerm_resource_group.data.name
+  location                      = azurerm_resource_group.data.location
+  kind                          = "MongoDB"
+  offer_type                    = "Standard"
+  tags                          = var.tags
+  public_network_access_enabled = false
 
   consistency_policy {
     consistency_level = "Eventual"
@@ -60,3 +61,38 @@ resource "azurerm_cosmosdb_mongo_collection" "pspconf" {
   }
 }
 
+#
+# PRIVATE ENDPOINT APP SUBNET -> COSMOS
+#
+resource "azurerm_private_dns_zone" "cosmos" {
+  name                = "privatelink.mongo.cosmos.azure.com"
+  resource_group_name = azurerm_resource_group.network.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "cosmos" {
+  name                  = azurerm_virtual_network.intern.name
+  resource_group_name   = azurerm_resource_group.network.name
+  private_dns_zone_name = azurerm_private_dns_zone.cosmos.name
+  virtual_network_id    = azurerm_virtual_network.intern.id
+}
+
+resource "azurerm_private_endpoint" "cosmos_pep" {
+  name                = "${local.project}-cosmos-pep"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+  subnet_id           = azurerm_subnet.app.id
+
+  custom_network_interface_name = "${local.project}-cosmos-pep-nic"
+
+  private_dns_zone_group {
+    name                 = "${local.project}-cosmos-pdzg"
+    private_dns_zone_ids = [azurerm_private_dns_zone.cosmos.id]
+  }
+
+  private_service_connection {
+    name                           = "${local.project}-cosmos-psc"
+    private_connection_resource_id = azurerm_cosmosdb_account.mil.id
+    subresource_names              = ["MongoDB"]
+    is_manual_connection           = false
+  }
+}
