d96c770ab0faf9a626750f8c6de8e7aca7ee2757
antoniotarricone <antonio.tarricone@pagopa.it>
2023-03-10 09:33:49 +0100

    Added identity.

--- src/core/04_storage.tf
+++ src/core/04_storage.tf
@@ -4,8 +4,17 @@ resource "azurerm_storage_account" "conf" {
   location                 = azurerm_resource_group.data.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  public_network_access_enabled = true
+  account_kind             = "StorageV2"
   tags                     = var.tags
+
+  #network_rules {
+  #  default_action             = "Deny"
+  #  virtual_network_subnet_ids = [azurerm_subnet.apim.id]
+  #}
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
 resource "azurerm_storage_container" "acquirer_conf" {
@@ -29,3 +38,39 @@ resource "azurerm_storage_blob" "acquirer_4585626" {
   type                   = "Block"
   source                 = "templates/4585625.json"
 }
+
+#
+# PRIVATE ENDPOINT APIM SUBNET -> STORAGE
+#
+#resource "azurerm_private_dns_zone" "storage" {
+#  name                = "privatelink.blob.core.windows.net"
+#  resource_group_name = azurerm_resource_group.network.name
+#}
+#
+#resource "azurerm_private_dns_zone_virtual_network_link" "storage" {
+#  name                  = azurerm_virtual_network.integr.name
+#  resource_group_name   = azurerm_resource_group.network.name
+#  private_dns_zone_name = azurerm_private_dns_zone.storage.name
+#  virtual_network_id    = azurerm_virtual_network.integr.id
+#}
+#
+#resource "azurerm_private_endpoint" "storage_pep" {
+#  name                = "${local.project}-storage-pep"
+#  location            = azurerm_resource_group.network.location
+#  resource_group_name = azurerm_resource_group.network.name
+#  subnet_id           = azurerm_subnet.apim.id
+#
+#  custom_network_interface_name = "${local.project}-storage-pep-nic"
+#
+#  private_dns_zone_group {
+#    name                 = "${local.project}-storage-pdzg"
+#    private_dns_zone_ids = [azurerm_private_dns_zone.storage.id]
+#  }
+#
+#  private_service_connection {
+#    name                           = "${local.project}-storage-psc"
+#    private_connection_resource_id = azurerm_storage_account.conf.id
+#    subresource_names              = ["blob"]
+#    is_manual_connection           = false
+#  }
+#}
