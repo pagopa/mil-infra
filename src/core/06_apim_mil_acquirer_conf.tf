3f459cdea4cb436415afa7b76d8b97d95bfcc393
antoniotarricone <antonio.tarricone@pagopa.it>
2023-03-10 09:36:46 +0100

    Added API diagnostic.

--- src/core/06_apim_mil_acquirer_conf.tf
+++ src/core/06_apim_mil_acquirer_conf.tf
@@ -6,10 +6,10 @@ module "acquirer_conf_api" {
   resource_group_name = module.apim.resource_group_name
   description         = "Handling the configuration of an Acquirer for Multi-channel Integration Layer of SW Client Project"
   protocols           = ["https"]
-  
+
   # Absolute URL of the backend service implementing this API.
   service_url = ""
-  
+
   # The Path for this API Management API, which is a relative URL which uniquely identifies this API and all of its resource paths within the API Management Service.
   path = "mil-acquirer-conf"
 
@@ -25,8 +25,67 @@ module "acquirer_conf_api" {
       xml_content = templatefile("policies/mil-acquirer-conf-getPspConfig.xml",
         {
           storage_container_uri = "https://${azurerm_storage_account.conf.name}.blob.core.windows.net/${azurerm_storage_container.acquirer_conf.name}/"
+          #storage_container_uri = "https://${azurerm_private_dns_zone.storage.name}/${azurerm_storage_container.acquirer_conf.name}/"
         }
       )
     }
   ]
+}
+
+resource "azurerm_api_management_api_diagnostic" "acquirer_conf_api" {
+  identifier               = "applicationinsights"
+  resource_group_name      = module.apim.resource_group_name
+  api_management_name      = module.apim.name
+  api_name                 = module.acquirer_conf_api.name
+  api_management_logger_id = module.apim.logger_id
+
+  sampling_percentage       = 100.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "verbose"
+  http_correlation_protocol = "W3C"
+
+  frontend_request {
+    body_bytes = 8192
+    headers_to_log = [
+      "RequestId",
+      "Version",
+      "AcquirerId",
+      "MerchantId",
+      "Channel",
+      "TerminalId",
+      "SessionId"
+    ]
+  }
+
+  frontend_response {
+    body_bytes = 8192
+    headers_to_log = [
+      "Location",
+      "Retry-After",
+      "Max-Retries"
+    ]
+  }
+
+  backend_request {
+    body_bytes = 8192
+    headers_to_log = [
+      "RequestId",
+      "Version",
+      "AcquirerId",
+      "MerchantId",
+      "Channel",
+      "TerminalId",
+      "SessionId"
+    ]
+  }
+
+  backend_response {
+    body_bytes = 8192
+    headers_to_log = [
+      "Location",
+      "Retry-After",
+      "Max-Retries"
+    ]
+  }
 }
