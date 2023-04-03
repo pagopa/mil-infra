becdcb624b5ae2e99770da9386e2d693af42df0d
fabrizio.guerrini <fabrizio.guerrini@C11-C9GE23PFRDO.dir.svc.accenture.com>
2023-04-03 14:50:54 +0200

    configuration for nodo mocks

--- src/core/04_storage__mock.tf
+++ src/core/04_storage__mock.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "mock" {
-  #count                    = var.env_short == "d" ? 1 : 0
+  count                    = var.env_short == "d" ? 1 : 0
   name                     = "mocknodo"
   resource_group_name      = azurerm_resource_group.data.name
   location                 = azurerm_resource_group.data.location
@@ -19,18 +19,62 @@ resource "azurerm_storage_account" "mock" {
 }
 
 resource "azurerm_storage_container" "mock" {
-  #count                 = var.env_short == "d" ? 1 : 0
+  count                 = var.env_short == "d" ? 1 : 0
   name                  = "mocknodo"
-  storage_account_name  = azurerm_storage_account.mock.name
+  storage_account_name  = azurerm_storage_account.mock[count.index].name
   container_access_type = "blob"
 }
 
-resource "azurerm_storage_blob" "response_1" {
+#### stubs returns KO for verify and activate ####
+resource "azurerm_storage_blob" "stub_verify_ko_activate_ko" {
   #count                  = var.env_short == "d" ? 1 : 0
-  name                   = "4585626.json" # File name on storage container
-  storage_account_name   = azurerm_storage_account.mock.name
-  storage_container_name = azurerm_storage_container.mock.name
+  for_each               = var.env_short == "d" ? toset([ "activatePaymentNotice_00000000001.xml","activatePaymentNotice_00000000002.xml","activatePaymentNotice_00000000003.xml",
+                                                          "activatePaymentNotice_00000000004.xml","activatePaymentNotice_00000000005.xml","activatePaymentNotice_00000000006.xml",
+                                                          "activatePaymentNotice_00000000007.xml","activatePaymentNotice_00000000008.xml","activatePaymentNotice_00000000009.xml",
+                                                          "verifyPaymentNotice_00000000001.xml","verifyPaymentNotice_00000000002.xml","verifyPaymentNotice_00000000003.xml",
+                                                          "verifyPaymentNotice_00000000004.xml","verifyPaymentNotice_00000000005.xml","verifyPaymentNotice_00000000006.xml",
+                                                          "verifyPaymentNotice_00000000007.xml","verifyPaymentNotice_00000000008.xml","verifyPaymentNotice_00000000009.xml"
+                                                          ]) : toset([])
+
+  name                   = each.key # File name on storage container
+  storage_account_name   = azurerm_storage_account.mock[0].name
+  storage_container_name = azurerm_storage_container.mock[0].name
   type                   = "Block"
-  source                 = "templates/4585625.json" # Local file name
+  source                 = join("",["mock-nodo/FiscalCode_0000000000x/",each.key]) # Local file name
 }
 
+#### stubs returns OK for verify and KO for activate ####
+resource "azurerm_storage_blob" "stub_verify_ok_activate_ko" {
+  #count                  = var.env_short == "d" ? 1 : 0
+  for_each               = var.env_short == "d" ? toset([ "activatePaymentNotice_00000000101.xml","activatePaymentNotice_00000000102.xml","activatePaymentNotice_00000000103.xml",
+                                                          "activatePaymentNotice_00000000104.xml","activatePaymentNotice_00000000105.xml","activatePaymentNotice_00000000106.xml",
+                                                          "activatePaymentNotice_00000000107.xml","activatePaymentNotice_00000000108.xml","activatePaymentNotice_00000000109.xml",
+                                                          "verifyPaymentNotice_00000000101.xml","verifyPaymentNotice_00000000102.xml","verifyPaymentNotice_00000000103.xml",
+                                                          "verifyPaymentNotice_00000000104.xml","verifyPaymentNotice_00000000105.xml","verifyPaymentNotice_00000000106.xml",
+                                                          "verifyPaymentNotice_00000000107.xml","verifyPaymentNotice_00000000108.xml","verifyPaymentNotice_00000000109.xml"
+                                                          ]) : toset([])
+  name                   = each.key # File name on storage container
+  storage_account_name   = azurerm_storage_account.mock[0].name
+  storage_container_name = azurerm_storage_container.mock[0].name
+  type                   = "Block"
+  source                 = join("",["mock-nodo/FiscalCode_0000000010x/",each.key]) # Local file name
+}
+
+
+#### stubs returns OK for verify and activate ####
+resource "azurerm_storage_blob" "stub_verify_ok_activate_ok" {
+  #count                  = var.env_short == "d" ? 1 : 0
+  for_each               = var.env_short == "d" ? toset([ "activatePaymentNotice_00000000201.xml","activatePaymentNotice_00000000202.xml","activatePaymentNotice_00000000203.xml",
+                                                          "activatePaymentNotice_00000000204.xml","activatePaymentNotice_00000000205.xml","activatePaymentNotice_00000000206.xml",
+                                                          "activatePaymentNotice_00000000207.xml","activatePaymentNotice_00000000208.xml","activatePaymentNotice_00000000209.xml",
+                                                          "verifyPaymentNotice_00000000201.xml","verifyPaymentNotice_00000000202.xml","verifyPaymentNotice_00000000203.xml",
+                                                          "verifyPaymentNotice_00000000204.xml","verifyPaymentNotice_00000000205.xml","verifyPaymentNotice_00000000206.xml",
+                                                          "verifyPaymentNotice_00000000207.xml","verifyPaymentNotice_00000000208.xml","verifyPaymentNotice_00000000209.xml",
+                                                          "outcome_4e2581e0ca454011830fe8f7fbd03a85.json","outcome_37b0bb69ce774bc2bfeb485e107ae335.json","payments.json"
+                                                          ]) : toset([])
+  name                   = join("",["/0000000020x/",each.key]) # File name on storage container
+  storage_account_name   = azurerm_storage_account.mock[0].name
+  storage_container_name = azurerm_storage_container.mock[0].name
+  type                   = "Block"
+  source                 = join("",["mock-nodo/FiscalCode_0000000020x/",each.key]) # Local file name
+}
---
+++ src/core/06_apim_mil_mock_nodo.tf
@@ -0,0 +1,36 @@
+# API for mil-acquirer-conf.
+module "mock_nodo_api" {
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v5.1.0"
+  name                = "${local.project}-mock-nodo"
+  api_management_name = module.apim.name
+  resource_group_name = module.apim.resource_group_name
+  description         = "Handling the configuration for mock Soap and Rest responses"
+  protocols           = ["https"]
+
+  # Absolute URL of the backend service implementing this API.
+  service_url = "https://api.uat.platform.pagopa.it/nodo/nodo-per-pm/v1"
+
+  # The Path for this API Management API, which is a relative URL which uniquely identifies this API and all of its resource paths within the API Management Service.
+  path = "mockNodo"
+
+  display_name          = "MockNodo"
+  #content_format        = "openapi-link"
+  #content_value         = "https://raw.githubusercontent.com/pagopa/mil-apis/main/openapi-mono/acquirer-conf.yaml"
+  content_format        = "openapi"
+  content_value         = file("${path.module}/mock-nodo/mock-nodo-openapi.yaml")
+  product_ids           = [module.mil_product.product_id]
+  subscription_required = false
+
+  api_operation_policies = [
+    {
+      operation_id = "mockSoap"
+      xml_content = templatefile("policies/mil-mock-nodo-soap.xml",{})
+    },
+    {
+      operation_id = "mockRest"
+      xml_content = templatefile("policies/mil-mock-nodo-rest.xml",{})
+    }
+  ]
+
+  
+}
