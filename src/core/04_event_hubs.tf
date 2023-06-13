108a53d5ced1959d67c2218bf439ff005dfb2eda
antoniotarricone <antonio.tarricone@pagopa.it>
2023-06-13 17:23:48 +0200

    Added EventHubs.

---
+++ src/core/04_event_hubs.tf
@@ -0,0 +1,20 @@
+resource "azurerm_eventhub_namespace" "mil_evhns" {
+  name                          = "${local.project}-evhns"
+  resource_group_name           = azurerm_resource_group.data.name
+  location                      = azurerm_resource_group.data.location
+  sku                           = "Standard"
+  capacity                      = 1
+  local_authentication_enabled  = true
+  zone_redundant                = true
+  public_network_access_enabled = true
+  auto_inflate_enabled          = false
+  tags                          = var.tags
+}
+
+resource "azurerm_eventhub" "presets" {
+  name                = "${local.project}-presets-evh"
+  namespace_name      = azurerm_eventhub_namespace.mil_evhns.name
+  resource_group_name = azurerm_eventhub_namespace.mil_evhns.resource_group_name
+  partition_count     = 1
+  message_retention   = 7
+}
