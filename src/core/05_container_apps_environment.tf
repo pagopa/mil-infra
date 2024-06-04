475488cb108a2d67398fd5ef559534a07805bd57
antoniotarricone <antonio.tarricone@pagopa.it>
2024-06-04 17:20:30 +0200

    Adjustment to new version of mil-auth.

--- src/core/05_container_apps_environment.tf
+++ src/core/05_container_apps_environment.tf
@@ -6,8 +6,39 @@ resource "azurerm_container_app_environment" "mil" {
   location                       = azurerm_resource_group.app.location
   resource_group_name            = azurerm_resource_group.app.name
   log_analytics_workspace_id     = azurerm_log_analytics_workspace.log_analytics_workspace.id
-  internal_load_balancer_enabled = false
+  internal_load_balancer_enabled = false # true
   infrastructure_subnet_id       = azurerm_subnet.app.id
   tags                           = var.tags
   zone_redundancy_enabled        = false
 }
+
+# ------------------------------------------------------------------------------
+# Private DNS to resolve Container Apps IP.
+# ------------------------------------------------------------------------------
+#resource "azurerm_private_dns_zone" "mil_cae" {
+#  name                = azurerm_container_app_environment.mil.default_domain
+#  resource_group_name = azurerm_resource_group.network.name
+#}
+
+#resource "azurerm_private_dns_zone_virtual_network_link" "mil_cae" {
+#  name                  = azurerm_virtual_network.intern.name
+#  resource_group_name   = azurerm_resource_group.network.name
+#  private_dns_zone_name = azurerm_private_dns_zone.mil_cae.name
+#  virtual_network_id    = azurerm_virtual_network.intern.id
+#}
+
+#resource "azurerm_private_dns_zone_virtual_network_link" "apim" {
+#  name                  = azurerm_virtual_network.integr.name
+#  resource_group_name   = azurerm_resource_group.network.name
+#  private_dns_zone_name = azurerm_private_dns_zone.mil_cae.name
+#  virtual_network_id    = azurerm_virtual_network.integr.id
+#}
+
+#resource "azurerm_private_dns_a_record" "mil_cae" {
+#  name                = "*"
+#  zone_name           = azurerm_private_dns_zone.mil_cae.name
+#  resource_group_name = azurerm_resource_group.network.name
+#  ttl                 = 300
+#  records             = [azurerm_container_app_environment.mil.static_ip_address]
+#}
+
