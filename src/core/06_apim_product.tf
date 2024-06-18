42c93eab5239518bbbc2ded1132fbbe528691999
antoniotarricone <antonio.tarricone@pagopa.it>
2024-06-18 12:58:56 +0200

    Added CORS policy for DEV and UAT.

--- src/core/06_apim_product.tf
+++ src/core/06_apim_product.tf
@@ -10,3 +10,33 @@ resource "azurerm_api_management_product" "mil" {
   subscription_required = false
   published             = true
 }
+
+# ------------------------------------------------------------------------------
+# CORS Policy.
+# ------------------------------------------------------------------------------
+resource "azurerm_api_management_product_policy" "example" {
+  count               = var.env_short != "p" ? 1 : 0
+  product_id          = azurerm_api_management_product.mil.product_id
+  api_management_name = azurerm_api_management_product.mil.api_management_name
+  resource_group_name = azurerm_api_management_product.mil.resource_group_name
+  xml_content = <<XML
+    <policies>
+      <inbound>
+        <cors allow-credentials="false">
+          <allowed-origins>
+            <origin>*</origin>
+          </allowed-origins>
+          <allowed-methods>
+            <method>GET</method>
+            <method>POST</method>
+          </allowed-methods>
+        </cors>
+      </inbound>
+      <backend>
+        <forward-request />
+      </backend>
+      <outbound />
+      <on-error />
+    </policies>
+  XML
+}
